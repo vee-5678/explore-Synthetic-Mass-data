Fix bug with age calculation and remove unnecessary config

code/main.py
@@ -5,29 +5,36 @@ import os
 import configparser
 
 ########################################################################################################################
-# Setup - read in config file
+# Setup
 ########################################################################################################################
-config = configparser.ConfigParser()
-config.read(Path(Path(os.getcwd()), 'config', 'config.ini'))
-
-display_options_config = config['display_options']
-file_path_config = config['file_paths']
-
 # Display options
-pd.set_option('display.width', display_options_config.getint('desired_width'))
-pd.set_option('display.max_columns', display_options_config.getint('max_columns'))
+pd.set_option('display.width', 180)
+pd.set_option('display.max_columns', 10)
+
+
+########################################################################################################################
+# Variables
+########################################################################################################################
+input_data_fp = Path('C:/', 'Users', '60226589', 'OneDrive - NSW Health Department', 'synthea_1m_fhir_3_0_May_24')
+# Based on files modified, data extracted in 2017 (31/12 for simplicity); used to calculate age for pts with no deathdate
+assumed_date_of_extract = '2017-12-31'
 
 ########################################################################################################################
 # Functions
 ########################################################################################################################
-
+def derive_age(birthdate, deathdate):
+    """Simple function to calculate age and handle where patient is still alive"""
+    end_date_to_use = pd.to_datetime(assumed_date_of_extract) if pd.isnull(deathdate) else deathdate
+    age = ((end_date_to_use - birthdate) / np.timedelta64(1, 'Y'))
+    # Round to 2 d.p.
+    age = round(age, 2)
+    
+    return age
 
 
 ########################################################################################################################
 # Import data
 ########################################################################################################################
-input_data_fp = Path(file_path_config['input_data_fp'])
-
 raw_data_file_list = [f for f in input_data_fp.glob('**/*') if f.is_file()]
 
 data_of_interest = ['patients',
@@ -46,7 +53,6 @@ raw_data = {'patients': pd.DataFrame(),
             'organizations': pd.DataFrame()}
 
 for file in raw_data_file_list:
-    print(file)
     # CSVs named the same, just different folders, so get the stem of the file which will indicate which DF to concat to
     data_category = file.stem
     if data_category not in data_of_interest:
@@ -59,8 +65,9 @@ for file in raw_data_file_list:
             # Add to the appropriate DF based on the name of the file
             raw_data[data_category] = df.copy() if raw_data[data_category].empty else pd.concat([raw_data[data_category], df])
         except pd.errors.ParserError as e:
-            print("File unable to be parsed:", e)
+            print(f'{file.parent.name}/{file.name} unable to be parsed:', e)
             pass
+
 
 # Standardise column names across all datasets - lowercase
 for df in raw_data.values():
@@ -89,6 +96,7 @@ conditions = conditions.loc[conditions['patient'].isin(patients_to_include)].dro
 encounters = encounters.loc[encounters['patient'].isin(patients_to_include)].drop_duplicates()
 medications = medications.loc[medications['patient'].isin(patients_to_include)].drop_duplicates()
 procedures = procedures.loc[procedures['patient'].isin(patients_to_include)].drop_duplicates()
+del patients_to_include
 
 # Parse date fields
 patients[['birthdate', 'deathdate']] = patients[['birthdate', 'deathdate']].apply(pd.to_datetime, errors='raise', format='%Y-%m-%d')
@@ -97,12 +105,24 @@ encounters['date'] = pd.to_datetime(encounters['date'], errors='raise', format='
 medications[['start', 'stop']] = medications[['start', 'stop']].apply(pd.to_datetime, errors='raise', format='%Y-%m-%d')
 procedures['date'] = pd.to_datetime(procedures['date'], errors='raise', format='%Y-%m-%d')
 
+# Add year variables for looking at trends over time
+# Where there is a start and stop date, just use start date for simplicty
+conditions['year'] = conditions['start'].dt.year
+encounters['year'] = encounters['date'].dt.year
+medications['year'] = medications['start'].dt.year
+procedures['year'] = procedures['date'].dt.year
+
+# In procedures, "Documentation of current medications" seems to be very common, though likely a system / process requirement?
+# Not very useful for analysis though, so dropping it
+procedures = procedures.loc[procedures['description'].str.lower() != 'documentation of current medications']
+
+
 
 ########################################################################################################################
 # Transform - add useful variables
 ########################################################################################################################
 # Patient age, and age groups - set as date types first
-patients['age'] = round((patients['deathdate'] - patients['birthdate']) / np.timedelta64(1, 'Y'), 2)
+patients['age'] = patients.apply(lambda x: derive_age(x['birthdate'], x['deathdate']), axis=1)
 patients['age_group'] = pd.cut(patients['age'],
                                # Could use pd.interval_range though gives misleading labels which have overlapping values
                                bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, float('inf')],
@@ -151,5 +171,5 @@ patients = patients.merge(
 patients = patients.drop(columns=['patient_x', 'patient_y'])
 
 
-
-
+# Drop variables no longer required
+del raw_data, conditions_count_per_pt, encounters_count_per_pt, medications_count_per_pt, procedures_count_per_pt